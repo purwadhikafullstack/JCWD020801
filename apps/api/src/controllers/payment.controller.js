bf9dd2875eb9a50a4d55341da3abe34dc2d0ba12
Hafizh24 <hafizhcbs@gmail.com>
2024-01-30 17:28:45 +0700

    FF-14 feat: add checkout

---
+++ apps/api/src/controllers/payment.controller.js
@@ -0,0 +1,9 @@
+import Midtrans from 'midtrans-client';
+
+const snap = new Midtrans.Snap({
+  isProduction: false,
+  serverKey: process.env.MIDTRANS_SERVER_KEY,
+  clientKey: process.env.MIDTRANS_CLIENT_KEY,
+});
+
+console.log(snap);
