069c72a30b4641391d882969f778942a992e66d3
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-02-02 10:47:27 +0700

    Fix: Edit deploy config

--- deploy.config.js
+++ deploy.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "JCWDOL-0208-01", // Format JCWD-{batchcode}-{groupnumber}
-        script: "./projects/server/src/index.js",
+        script: "./apps/api/src/index.js",
         env: {
           NODE_ENV: "production",
           PORT: 2801,
