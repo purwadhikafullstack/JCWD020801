367c04ebc880d6c6dee8fbdd02f475b8b0da0d0a
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-02-02 10:53:34 +0700

    Development (#18)
    * Added admin login page (#1)
    
    * FF-13 feat:database (#2)
    
    * feat: add customer authentication and authorization (#4)
    
    Co-authored-by: thoby za <thobyy.athaya@gmail.com>
    
    * FF-8 feat: Admin management and authorization (#6)
    
    * FF-8 feat: admin management and  authorization
    
    * FF-8 feat: admin management and  authorization
    
    * Ff 2 (#7)
    
    * feat: add customer authentication and authorization
    
    * feat: user Authentication & Authorizaiton update
    
    * feat: user address, location, store management
    
    * fix: user dashboard update
    
    * Ff 13 (#8)
    
    * update database
    
    * FF-13 feat: add shopping cart
    
    * FF-13 feat: add cart
    
    * FF-13 feat: update shopping cart
    
    * Ff 4 (#10)
    
    * feat: add customer authentication and authorization
    
    * feat: user Authentication & Authorizaiton update
    
    * feat: user address, location, store management
    
    * fix: user dashboard update
    
    * fix: homepage and store locator
    
    * fix: checkout page
    
    * FF 9 Product Management (#11)
    
    * FF-8 feat: admin management and  authorization
    
    * FF-8 feat: admin management and  authorization
    
    * FF-9
    
    * [FF-9] feat: product management
    
    * Ff 5 (#12)
    
    * feat: add customer authentication and authorization
    
    * feat: user Authentication & Authorizaiton update
    
    * feat: user address, location, store management
    
    * fix: user dashboard update
    
    * fix: homepage and store locator
    
    * fix: checkout page
    
    * fix: address data
    
    * fix: google map api
    
    * Fix: Setup Deploy
    
    * Fix: Bug Fixing
    
    * Fix: Bug Fixing
    
    * Fix: Bug Fixing
    
    * Fix: Edit deploy config
    
    * Fix: Edit deploy config
    
    ---------
    
    Co-authored-by: dittolk <135496711+dittolk@users.noreply.github.com>
    Co-authored-by: hafizh khairuddin <95872736+Hafizh24@users.noreply.github.com>
    Co-authored-by: thoby za <thobyy.athaya@gmail.com>
    Co-authored-by: thoby za <99469497+thobyza@users.noreply.github.com>

--- deploy.config.js
+++ deploy.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: "JCWDOL-0208-01", // Format JCWD-{batchcode}-{groupnumber}
+        name: "JCWD-0208-01", // Format JCWD-{batchcode}-{groupnumber}
         script: "./apps/api/src/index.js",
         env: {
           NODE_ENV: "production",
